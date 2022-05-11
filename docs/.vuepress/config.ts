5dbeac23f8717bc41f070b25854471ab8fd3350e
JimFKppt <tj863020677@outlook.com>
2022-05-11 19:11:52 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -121,6 +121,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         items: [
           { text: 'Monthly', link: '/pages/455805/'},
           { text: 'Others', link: '/pages/bdd3a5/'},
+          { text: 'Khan Acadamy', link: '/pages/0da670/'},
         ],
       },
       {
@@ -131,7 +132,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '面试', link: '/pages/aea6571b7a8bae86/' },
           { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
           { text: '实用技巧', link: '/pages/baaa02/' },
-          { text: 'Khan Acadamy', link: '/pages/cb6dae/'},
           { text: '友情链接', link: '/friends/' },
         ],
       },
